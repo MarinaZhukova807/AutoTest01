2349452b9ca312f10486c81e3032b6d492619287
Марина Жукова <marinazhukova807@gmail.com>
2022-07-23 10:03:19 +0300

    add correct params for JUnit4 asserts

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,21 +11,21 @@ public class CashbackHackServiceTest {
     public void CashBackNoBonusTest(){
     int actual = bonus.remain(400);
     int expected = 600;
-    assertEquals(actual,expected);
+    assertEquals(expected, actual);
     }
 
     @Test
     public void CashBackOneBonusTest(){
         int actual = bonus.remain(1000);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void CashBackMoreBonusTest(){
         int actual = bonus.remain(1100);
         int expected = 900;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
 
 }
